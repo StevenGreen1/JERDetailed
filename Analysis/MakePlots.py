120abd081b2d395ad241dc7ffa54a24d3d034289
Steven Green <sg1sg2sg3@hotmail.co.uk>
2015-10-28 11:53:29 +0000

    Working plotting script.

--- Analysis/MakePlots.py
+++ Analysis/MakePlots.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+import os
 import sys
 import re
 import math
@@ -12,56 +13,91 @@ JER = defaultdict(dict)
 JERError = defaultdict(dict)
 
 pandoraSettings = ''
-pandoraSettingsList = ['Default','PerfectPhoton','PerfectPhotonNK0L','PerfectPFA','Muon']
+pandoraSettingsList = ['Default','PerfectPhoton','PerfectPhotonNK0L','PerfectPFA']
 jetEnergyList = [30, 40, 50, 60, 70, 80, 91, 100, 110, 120, 130, 140, 150, 160, 170, 180, 190, 200, 220, 240, 260, 280, 300, 350, 400, 450, 500]
-detectorModel = sys.argv[1]
-recoVar = sys.argv[2]
-inputFileName = 'Detector_Model_' + str(detectorModel) + '_Reco_Var_' + str(recoVar) + '_Results.txt'
+detectorModelList = [38]
+recoVarList = range(69,77)
+
+#===== What To Plot =====#
+
+NameOfPlot = 'Example'
+
+ToPlot = [
+             ['Default', 38, 69],
+             ['Default', 38, 71],
+             ['Default', 38, 76],
+             ['PhotonConfusion', 38, 73],
+             ['PerfectPFA', 38, 69]
+         ]
 
 #===== Read data =====#
 
-file = open(inputFileName)
-allLines = file.readlines()
-for line in allLines:
-    regex = re.compile("Detector Model (\d+)")
-    q = regex.search(line.strip()) 
-    if q is not None:
-        detectorModel = int(q.group(1))
+for detectorModel in detectorModelList:
+    for recoVar in recoVarList:
+        inputFileName = 'Detector_Model_' + str(detectorModel) + '_Reco_Var_' + str(recoVar) + '_Results.txt'
+        file = open(inputFileName)
+        allLines = file.readlines()
+        for line in allLines:
+            regex = re.compile("Detector Model (\d+)")
+            q = regex.search(line.strip()) 
+            if q is not None:
+                detectorModel = int(q.group(1))
 
-    if line.rstrip() in pandoraSettingsList:
-        pandoraSettings = line.rstrip() 
+            if line.rstrip() in pandoraSettingsList:
+                pandoraSettings = line.rstrip() 
 
-    regex = re.compile("(\d+) GeV Di Jet Energy:fPFA_L7A(.*?)sE\/E: (\d\.\d+)\+\-(\d\.\d+)")
-    r = regex.search(line)
-    if r is not None:
-        energy = int(r.group(1))
-        JER[(pandoraSettings,energy,detectorModel)] = float(r.group(3))
-        JERError[(pandoraSettings,energy,detectorModel)] = float(r.group(4))
+            regex = re.compile("(\d+) GeV Di Jet Energy:fPFA_L7A(.*?)sE\/E: (\d\.\d+)\+\-(\d\.\d+)")
+            r = regex.search(line)
+            if r is not None:
+                energy = int(r.group(1))
+                JER[(pandoraSettings,energy,detectorModel,recoVar)] = float(r.group(3))
+                JERError[(pandoraSettings,energy,detectorModel,recoVar)] = float(r.group(4))
 
 #===== Calculate Confusion Terms =====#
 
-# Total Confusion
-for energy in jetEnergyList:    
-    JER[('TotalConfusion',energy,detectorModel)] = math.sqrt( math.fabs( (JER[('Default',energy,detectorModel)] * JER[('Default',energy,detectorModel)]) - (JER[('PerfectPFA',energy,detectorModel)] * JER[('PerfectPFA',energy,detectorModel)]) ) )
-    JERError[('TotalConfusion',energy,detectorModel)] = math.sqrt( math.fabs( ( (JERError[('Default',energy,detectorModel)] * JERError[('Default',energy,detectorModel)] * JER[('Default',energy,detectorModel)] * JER[('Default',energy,detectorModel)]) / (JER[('TotalConfusion',energy,detectorModel)] * JER[('TotalConfusion',energy,detectorModel)]) ) - ( (JERError[('PerfectPFA',energy,detectorModel)] * JERError[('PerfectPFA',energy,detectorModel)] * JER[('PerfectPFA',energy,detectorModel)] * JER[('PerfectPFA',energy,detectorModel)]) / (JER[('TotalConfusion',energy,detectorModel)] * JER[('TotalConfusion',energy,detectorModel)]) ) ) )
+for recoVar in recoVarList:
+    # Total Confusion
+    for energy in jetEnergyList:    
+        JER[('TotalConfusion',energy,detectorModel,recoVar)] = math.sqrt( math.fabs( (JER[('Default',energy,detectorModel,recoVar)] * JER[('Default',energy,detectorModel,recoVar)]) - (JER[('PerfectPFA',energy,detectorModel,recoVar)] * JER[('PerfectPFA',energy,detectorModel,recoVar)]) ) )
+        JERError[('TotalConfusion',energy,detectorModel,recoVar)] = math.sqrt( math.fabs( ( (JERError[('Default',energy,detectorModel,recoVar)] * JERError[('Default',energy,detectorModel,recoVar)] * JER[('Default',energy,detectorModel,recoVar)] * JER[('Default',energy,detectorModel,recoVar)]) / (JER[('TotalConfusion',energy,detectorModel,recoVar)] * JER[('TotalConfusion',energy,detectorModel,recoVar)]) ) - ( (JERError[('PerfectPFA',energy,detectorModel,recoVar)] * JERError[('PerfectPFA',energy,detectorModel,recoVar)] * JER[('PerfectPFA',energy,detectorModel,recoVar)] * JER[('PerfectPFA',energy,detectorModel,recoVar)]) / (JER[('TotalConfusion',energy,detectorModel,recoVar)] * JER[('TotalConfusion',energy,detectorModel,recoVar)]) ) ) )
 
-# Photon Confusion
-for energy in jetEnergyList:    
-    JER[('PhotonConfusion',energy,detectorModel)] = math.sqrt( math.fabs( (JER[('Default',energy,detectorModel)] * JER[('Default',energy,detectorModel)]) - (JER[('PerfectPhoton',energy,detectorModel)] * JER[('PerfectPhoton',energy,detectorModel)]) ) )
-    JERError[('PhotonConfusion',energy,detectorModel)] = math.sqrt( math.fabs( ( (JERError[('Default',energy,detectorModel)] * JERError[('Default',energy,detectorModel)] * JER[('Default',energy,detectorModel)] * JER[('Default',energy,detectorModel)]) / (JER[('PhotonConfusion',energy,detectorModel)] * JER[('PhotonConfusion',energy,detectorModel)]) ) - ( (JERError[('PerfectPhoton',energy,detectorModel)] * JERError[('PerfectPhoton',energy,detectorModel)] * JER[('PerfectPhoton',energy,detectorModel)] * JER[('PerfectPhoton',energy,detectorModel)]) / (JER[('PhotonConfusion',energy,detectorModel)] * JER[('PhotonConfusion',energy,detectorModel)]) ) ) )
+    # Photon Confusion
+    for energy in jetEnergyList:    
+        JER[('PhotonConfusion',energy,detectorModel,recoVar)] = math.sqrt( math.fabs( (JER[('Default',energy,detectorModel,recoVar)] * JER[('Default',energy,detectorModel,recoVar)]) - (JER[('PerfectPhoton',energy,detectorModel,recoVar)] * JER[('PerfectPhoton',energy,detectorModel,recoVar)]) ) )
+        JERError[('PhotonConfusion',energy,detectorModel,recoVar)] = math.sqrt( math.fabs( ( (JERError[('Default',energy,detectorModel,recoVar)] * JERError[('Default',energy,detectorModel,recoVar)] * JER[('Default',energy,detectorModel,recoVar)] * JER[('Default',energy,detectorModel,recoVar)]) / (JER[('PhotonConfusion',energy,detectorModel,recoVar)] * JER[('PhotonConfusion',energy,detectorModel,recoVar)]) ) - ( (JERError[('PerfectPhoton',energy,detectorModel,recoVar)] * JERError[('PerfectPhoton',energy,detectorModel,recoVar)] * JER[('PerfectPhoton',energy,detectorModel,recoVar)] * JER[('PerfectPhoton',energy,detectorModel,recoVar)]) / (JER[('PhotonConfusion',energy,detectorModel,recoVar)] * JER[('PhotonConfusion',energy,detectorModel,recoVar)]) ) ) )
 
-# Neutral Hadron Confusion
-for energy in jetEnergyList:
-    JER[('NeutralHadronConfusion',energy,detectorModel)] = math.sqrt( math.fabs( (JER[('PerfectPhoton',energy,detectorModel)] * JER[('PerfectPhoton',energy,detectorModel)]) - (JER[('PerfectPhotonNK0L',energy,detectorModel)] * JER[('PerfectPhotonNK0L',energy,detectorModel)]) ) )    
-    JERError[('NeutralHadronConfusion',energy,detectorModel)] = math.sqrt( math.fabs( ( (JERError[('PerfectPhoton',energy,detectorModel)] * JERError[('PerfectPhoton',energy,detectorModel)] * JER[('PerfectPhoton',energy,detectorModel)] * JER[('PerfectPhoton',energy,detectorModel)]) / (JER[('NeutralHadronConfusion',energy,detectorModel)] * JER[('NeutralHadronConfusion',energy,detectorModel)]) ) - ( (JERError[('PerfectPhotonNK0L',energy,detectorModel)] * JERError[('PerfectPhotonNK0L',energy,detectorModel)] * JER[('PerfectPhotonNK0L',energy,detectorModel)] * JER[('PerfectPhotonNK0L',energy,detectorModel)]) / (JER[('NeutralHadronConfusion',energy,detectorModel)] * JER[('NeutralHadronConfusion',energy,detectorModel)]) ) ) )
+    # Neutral Hadron Confusion
+    for energy in jetEnergyList:
+        JER[('NeutralHadronConfusion',energy,detectorModel,recoVar)] = math.sqrt( math.fabs( (JER[('PerfectPhoton',energy,detectorModel,recoVar)] * JER[('PerfectPhoton',energy,detectorModel,recoVar)]) - (JER[('PerfectPhotonNK0L',energy,detectorModel,recoVar)] * JER[('PerfectPhotonNK0L',energy,detectorModel,recoVar)]) ) )    
+        JERError[('NeutralHadronConfusion',energy,detectorModel,recoVar)] = math.sqrt( math.fabs( ( (JERError[('PerfectPhoton',energy,detectorModel,recoVar)] * JERError[('PerfectPhoton',energy,detectorModel,recoVar)] * JER[('PerfectPhoton',energy,detectorModel,recoVar)] * JER[('PerfectPhoton',energy,detectorModel,recoVar)]) / (JER[('NeutralHadronConfusion',energy,detectorModel,recoVar)] * JER[('NeutralHadronConfusion',energy,detectorModel,recoVar)]) ) - ( (JERError[('PerfectPhotonNK0L',energy,detectorModel,recoVar)] * JERError[('PerfectPhotonNK0L',energy,detectorModel,recoVar)] * JER[('PerfectPhotonNK0L',energy,detectorModel,recoVar)] * JER[('PerfectPhotonNK0L',energy,detectorModel,recoVar)]) / (JER[('NeutralHadronConfusion',energy,detectorModel,recoVar)] * JER[('NeutralHadronConfusion',energy,detectorModel,recoVar)]) ) ) )
 
-# Other Confusion
-for energy in jetEnergyList:
-    JER[('OtherConfusion',energy,detectorModel)] = math.sqrt( math.fabs( (JER[('PerfectPhotonNK0L',energy,detectorModel)] * JER[('PerfectPhotonNK0L',energy,detectorModel)]) - (JER[('PerfectPFA',energy,detectorModel)] * JER[('PerfectPFA',energy,detectorModel)]) ) )    
-    JERError[('OtherConfusion',energy,detectorModel)] = math.sqrt( math.fabs( ( (JERError[('PerfectPhotonNK0L',energy,detectorModel)] * JERError[('PerfectPhotonNK0L',energy,detectorModel)] * JER[('PerfectPhotonNK0L',energy,detectorModel)] * JER[('PerfectPhotonNK0L',energy,detectorModel)]) / (JER[('OtherConfusion',energy,detectorModel)] * JER[('OtherConfusion',energy,detectorModel)]) ) - ( (JERError[('PerfectPFA',energy,detectorModel)] * JERError[('PerfectPFA',energy,detectorModel)] * JER[('PerfectPFA',energy,detectorModel)] * JER[('PerfectPFA',energy,detectorModel)]) / (JER[('OtherConfusion',energy,detectorModel)] * JER[('OtherConfusion',energy,detectorModel)]) ) ) )
+    # Other Confusion
+    for energy in jetEnergyList:
+        JER[('OtherConfusion',energy,detectorModel,recoVar)] = math.sqrt( math.fabs( (JER[('PerfectPhotonNK0L',energy,detectorModel,recoVar)] * JER[('PerfectPhotonNK0L',energy,detectorModel,recoVar)]) - (JER[('PerfectPFA',energy,detectorModel,recoVar)] * JER[('PerfectPFA',energy,detectorModel,recoVar)]) ) )    
+        JERError[('OtherConfusion',energy,detectorModel,recoVar)] = math.sqrt( math.fabs( ( (JERError[('PerfectPhotonNK0L',energy,detectorModel,recoVar)] * JERError[('PerfectPhotonNK0L',energy,detectorModel,recoVar)] * JER[('PerfectPhotonNK0L',energy,detectorModel,recoVar)] * JER[('PerfectPhotonNK0L',energy,detectorModel,recoVar)]) / (JER[('OtherConfusion',energy,detectorModel,recoVar)] * JER[('OtherConfusion',energy,detectorModel,recoVar)]) ) - ( (JERError[('PerfectPFA',energy,detectorModel,recoVar)] * JERError[('PerfectPFA',energy,detectorModel,recoVar)] * JER[('PerfectPFA',energy,detectorModel,recoVar)] * JER[('PerfectPFA',energy,detectorModel,recoVar)]) / (JER[('OtherConfusion',energy,detectorModel,recoVar)] * JER[('OtherConfusion',energy,detectorModel,recoVar)]) ) ) )
 
 #for x in JER.keys():print x,' : ',JER[x]
 #for x in JERError.keys():print x,' : ',JERError[x]
+
+#==== Tools ====#
+
+def rootLineColor(pandora):
+    colourDict = defaultdict(dict)
+    colourDict = {'Default':'kBlack', 'PerfectPFA':'kBlue', 'TotalConfusion':'kRed', 'PhotonConfusion':'kOrange', 'NeutralHadronConfusion':'8', 'OtherConfusion':'kMagenta'}
+    # Neutral had is green 
+    return colourDict[(pandora)]
+
+#rootLineStyle = ['1','2','3','4','5','6']
+
+def rootMarkerStyle(recoVar):
+    markerDict = defaultdict(dict)
+    markerDict = {69:20, 70:21, 71:22, 72:23, 73:24, 75:25, 76:26}
+    return markerDict[(recoVar)]
+
+def truncationFromRecoVar(recoVar):
+    truncDict = defaultdict(dict)
+    truncDict = {69:0.5, 70:0.75, 71:1, 72:1.5, 73:2, 74:5, 75:10, 76:1000000}
+    return truncDict[(recoVar)]
 
 #==== Make JER_vs_Ej =====#
 
@@ -91,48 +127,54 @@ for energy in jetEnergyList:
         saveString += ','
 saveString += '};\n'
 
-for pandoraSettings in ['Default','PerfectPFA','TotalConfusion','PhotonConfusion','NeutralHadronConfusion','OtherConfusion']:
-    saveString += 'float ' + pandoraSettings + '_' + str(detectorModel) + '_JER[' + str(len(jetEnergyList)) + '] = {'
-    for energy in jetEnergyList:
-        saveString += str(JER[(pandoraSettings,energy,detectorModel)])
-        if energy is not jetEnergyList[-1]:
-            saveString += ','
-    saveString += '};\n'
-    saveString += 'float ' + pandoraSettings + '_' + str(detectorModel) + '_JERError[' + str(len(jetEnergyList)) + '] = {'
-    for energy in jetEnergyList:
-        saveString += str(JERError[(pandoraSettings,energy,detectorModel)])
-        if energy is not jetEnergyList[-1]:
-            saveString += ','
-    saveString += '};\n'
-
-rootLineColor = ['1','4','2','kOrange','8','6']
-rootLineStyle = ['1','2','3','4','5','6']
-
 saveString += 'TLegend *pLegend = new TLegend(0.6, 0.6, 0.9, 0.9);\n'
 
-settingsCounter = 0
-for pandoraSettings in ['Default','PerfectPFA','TotalConfusion','PhotonConfusion','NeutralHadronConfusion','OtherConfusion']:
-    saveString += 'TGraphErrors *pTGraphErrors_' + pandoraSettings + '_' + str(detectorModel) + ' = new TGraphErrors(' + str(len(jetEnergyList)) + ',jetEnergy,' + pandoraSettings + '_' + str(detectorModel) + '_JER,jetEnergyError,' + pandoraSettings + '_' + str(detectorModel) + '_JERError);\n'
-    saveString += 'pTGraphErrors_' + pandoraSettings + '_' + str(detectorModel) + '->SetLineColor(' + rootLineColor[settingsCounter] + ');\n'
-    saveString += 'pTGraphErrors_' + pandoraSettings + '_' + str(detectorModel) + '->SetMarkerColor(' + rootLineColor[settingsCounter] + ');\n'
-    saveString += 'pTGraphErrors_' + pandoraSettings + '_' + str(detectorModel) + '->Draw("lp,same");\n'
-    settingsCounter = settingsCounter + 1
+for item in ToPlot:
+    pandoraToPlot = item[0]
+    detectorModelToPlot = item[1]
+    recoVarToPlot = item[2]
 
-for pandoraSettings in ['Default','PerfectPFA','TotalConfusion','PhotonConfusion','NeutralHadronConfusion','OtherConfusion']:
-    saveString += 'pLegend->AddEntry(pTGraphErrors_' + pandoraSettings + '_' + str(detectorModel) + ', "' + pandoraSettings + '", "lp");\n'
+    saveString += 'float ' + pandoraToPlot + '_' + str(detectorModelToPlot) + '_' + str(recoVarToPlot) + '_JER[' + str(len(jetEnergyList)) + '] = {'
+    for energy in jetEnergyList:
+        saveString += str(JER[(pandoraToPlot,energy,detectorModelToPlot,recoVarToPlot)])
+        if energy is not jetEnergyList[-1]:
+            saveString += ','
+    saveString += '};\n'
+
+    saveString += 'float ' + pandoraToPlot + '_' + str(detectorModelToPlot) + '_' + str(recoVarToPlot) + '_JERError[' + str(len(jetEnergyList)) + '] = {'
+    for energy in jetEnergyList:
+        saveString += str(JERError[(pandoraToPlot,energy,detectorModelToPlot,recoVarToPlot)])
+        if energy is not jetEnergyList[-1]:
+            saveString += ','
+    saveString += '};\n'
+
+    saveString += 'TGraphErrors *pTGraphErrors_' + pandoraToPlot + '_' + str(detectorModelToPlot) + '_' + str(recoVarToPlot) + ' = new TGraphErrors(' + str(len(jetEnergyList)) + ',jetEnergy,' + pandoraToPlot + '_' + str(detectorModelToPlot) + '_' + str(recoVarToPlot) + '_JER,jetEnergyError,' + pandoraToPlot + '_' + str(detectorModelToPlot) + '_' + str(recoVarToPlot) + '_JERError);\n'
+    saveString += 'pTGraphErrors_' + pandoraToPlot + '_' + str(detectorModelToPlot) + '_' + str(recoVarToPlot) + '->SetLineColor(' + rootLineColor(pandoraToPlot) + ');\n'
+    saveString += 'pTGraphErrors_' + pandoraToPlot + '_' + str(detectorModelToPlot) + '_' + str(recoVarToPlot) + '->SetMarkerColor(' + rootLineColor(pandoraToPlot) + ');\n'
+    saveString += 'pTGraphErrors_' + pandoraToPlot + '_' + str(detectorModelToPlot) + '_' + str(recoVarToPlot) + '->SetMarkerStyle(' + str(rootMarkerStyle(recoVarToPlot)) + ');\n'
+    saveString += 'pTGraphErrors_' + pandoraToPlot + '_' + str(detectorModelToPlot) + '_' + str(recoVarToPlot) + '->Draw("lp,same");\n'
+    legendDescription = pandoraToPlot + ', Hadronic Energy Truncation ' + str(truncationFromRecoVar(recoVarToPlot))
+    saveString += 'pLegend->AddEntry(pTGraphErrors_' + pandoraToPlot + '_' + str(detectorModelToPlot) + '_' + str(recoVarToPlot) + ', "' + legendDescription + '", "lp");\n'
 
 saveString += 'pLegend->SetFillStyle(0);\n'
 saveString += 'pLegend->Draw("same");\n'
 
-saveString += 'pCanvasEj->SaveAs("JER_vs_Ej_Detector_Model_' + str(detectorModel) + '_Reco_Var_' + str(recoVar) + '.pdf");\n'
+fileName  = 'JER_vs_Ej_' + NameOfPlot + '.pdf'
+if os.path.isfile(fileName): 
+    print 'File ' + fileName + ' already exists!'
+    sys.exit()
+
+saveString += 'pCanvasEj->SaveAs("' + fileName + '");\n'
 saveString += '}\n'
 
 #===== Write out file =====#
-resultsFile = open('JER_vs_Ej_Detector_Model_' + str(detectorModel) + '_Reco_Var_' + str(recoVar) + '.C', 'w')
+
+fileName  = 'JER_vs_Ej_' + NameOfPlot + '.C'
+if os.path.isfile(fileName):
+    print 'File ' + fileName + ' already exists!'
+    sys.exit()
+
+resultsFile = open('JER_vs_Ej_' + NameOfPlot + '.C', 'w')
 resultsFile.write(saveString)
 resultsFile.close()
-
-detectorModel = 38
-recoVar = 69
-
 
